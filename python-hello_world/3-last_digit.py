8cd4d63a8eda43104c0e1de14147a6f731981fa9
kejgon james <kejgon@gmail.com>
2023-11-27 15:11:47 +0300

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -4,8 +4,11 @@ import random
 number = random.randint(-10000, 10000)
 
 # Extract the last digit with sign
-last_digit = number % 10 if number >= 0 else -(-number % 10)
-
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = -(-number % 10)
+    
 # Initialize an empty string to hold the classification
 classification = ""
 
