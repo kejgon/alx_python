03b97e5864293df8397f58f9f818b4c54c78c85b
kejgon james <kejgon@gmail.com>
2023-11-27 13:51:54 +0300

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,16 +1,22 @@
 import random
-
 number = random.randint(-10000, 10000)
-last_digit = number % 10
+# Extract the last digit
+last_digit = abs(number) % 10
 
-print("Last digit of", number, "is", last_digit)
+# Extract the last digit
+last_digit = abs(number) % 10
 
+
+
+# Classify the last digit
+classification = ""
+# Classify the last digit
 if last_digit > 5:
-    if last_digit < 10:
-        print("and is greater than 5")
-
+    classification = "and is greater than 5"
 if last_digit == 0:
-    print("and is 0")
+    classification = "and is 0"
+if last_digit != 0 and last_digit < 6:
+    classification = "and is less than 6 and not 0"
 
-if last_digit < 6 and last_digit != 0:
-    print("and is less than 6 and not 0")
+# Print the result
+print("Last digit of {} is {} {}".format(number, last_digit, classification))
