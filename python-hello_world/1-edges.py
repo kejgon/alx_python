d26e01f0397431b013638a295468e7a2180d8b6b
kejgon james <kejgon@gmail.com>
2023-11-27 10:44:50 +0300

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word = "Holberton"
 word_first_3 = word[0:3]
-print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
-print("Last 2 letters: {}".format(word_last_2))
 middle_index = len(word) // 2
-middle_word = word[middle_index - 3:middle_index + 6]  # Adjusted the slice to capture "olberto"
+middle_word = word[middle_index - 3:middle_index + 4]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
