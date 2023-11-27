5d323a19333de33453db7bb81e9e80bd6d80ef22
kejgon james <kejgon@gmail.com>
2023-11-27 14:06:28 +0300

    commit

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,4 @@
 for i in range(10):
     for j in range(i + 1, 10):
         combination = i * 10 + j
-        print("{:02}, ".format(combination), end="" if j < 9 else "\n")
+        print("{:02}, ".format(combination), end="" if j < 9 else "\n", flush=True)
