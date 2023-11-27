54742fc38880818881235acae65b4864c17ef01a
kejgon james <kejgon@gmail.com>
2023-11-27 13:37:20 +0300

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -9,12 +9,14 @@ last_digit = abs(number) % 10
 
 
 # Classify the last digit
+classification = ""
+
 if last_digit > 5:
     classification = "and is greater than 5"
-if last_digit == 0:
+elif last_digit == 0:
     classification = "and is 0"
-if 0 < last_digit < 6:
+else:
     classification = "and is less than 6 and not 0"
 
 # Print the result
-print("The string Last digit of, followed by\nthe number, followed by\nthe string is, followed by the last digit of number,", classification)
+print("Last digit of {} is {} {}".format(number, last_digit, classification))
