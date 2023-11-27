e1bceaabe7645e4b4942f9f95a2b69bbd429d339
kejgon james <kejgon@gmail.com>
2023-11-27 10:35:08 +0300

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -3,5 +3,6 @@ word_first_3 = word[0:3]
 print("First 3 letters: {}".format(word_first_3))
 word_last_2 = word[-2:]
 print("Last 2 letters: {}".format(word_last_2))
-middle_word = word[3:6]
+middle_index = len(word) // 2
+middle_word = word[middle_index - 1:middle_index + 5]
 print("Middle word: {}".format(middle_word))
