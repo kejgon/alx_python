96256d3ac15eeddcd232076f6dacc88c4e5949f9
kejgon james <kejgon@gmail.com>
2023-11-28 14:26:11 +0300

    commit

---
+++ python-functions/3-string.py
@@ -0,0 +1,8 @@
+def reverse_string(input_string):
+    reversed_string = input_string[::-1]
+    return reversed_string
+
+reverse_string("Hello")
+reverse_string("")
+reverse_string("madam")
+reverse_string("Hello, World!")
