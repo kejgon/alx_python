6a93b2aa0bfed0fb636b4418162e99551b25cf08
kejgon james <kejgon@gmail.com>
2023-11-28 13:56:25 +0300

    commit

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,6 +2,8 @@ def convert_to_celsius(fahrenheit):
     celsius = (5 / 9) * (fahrenheit - 32)
     return celsius
 
+
+
 convert_to_celsius(100)
 convert_to_celsius(-40)
 convert_to_celsius(-459.67)
