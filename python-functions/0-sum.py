53543d4ab2ef1ad98b2eed032c89bd9aacb90056
kejgon james <kejgon@gmail.com>
2023-11-28 13:32:43 +0300

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -2,8 +2,20 @@ def add(a, b):
     return a+b 
     
 
-add = __import__('0-sum').add
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+# Case: add(1, 2)
+result_case_1 = add(1, 2)
+print("Correct output - case: add(1, 2)\n", result_case_1)
+
+# Case: add(100, -2)
+result_case_2 = add(100, -2)
+print("\nCorrect output - case: add(100, -2)\n", result_case_2)
+
+# Case: add(-100, -2)
+result_case_3 = add(-100, -2)
+print("\nCorrect output - case: add(-100, -2)\n", result_case_3)
+
+# Case: add(0, 0)
+result_case_4 = add(0, 0)
+print("\nCorrect output - case: add(0, 0)\n", result_case_4)
+
