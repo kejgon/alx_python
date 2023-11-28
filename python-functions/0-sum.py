3b2dc9e904859ece94972be239f54c4460b0f998
kejgon james <kejgon@gmail.com>
2023-11-28 13:33:57 +0300

    commit

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -5,17 +5,17 @@ def add(a, b):
 
 # Case: add(1, 2)
 result_case_1 = add(1, 2)
-print("Correct output - case: add(1, 2)\n", result_case_1)
+print(result_case_1)
 
 # Case: add(100, -2)
 result_case_2 = add(100, -2)
-print("\nCorrect output - case: add(100, -2)\n", result_case_2)
+print(result_case_2)
 
 # Case: add(-100, -2)
 result_case_3 = add(-100, -2)
-print("\nCorrect output - case: add(-100, -2)\n", result_case_3)
+print(result_case_3)
 
 # Case: add(0, 0)
 result_case_4 = add(0, 0)
-print("\nCorrect output - case: add(0, 0)\n", result_case_4)
+print(result_case_4)
 
