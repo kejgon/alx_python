1f668bacacf579db4c8569d9bbad28ce14b79425
kejgon james <kejgon@gmail.com>
2023-11-28 11:40:29 +0300

    commit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -27,9 +27,14 @@
 
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(str(number)[-1])
-if number < 0:
-    last_digit = last_digit * int(-1)
+# last_digit = int(str(number)[-1])
+# if number < 0:
+#     last_digit = last_digit * int(-1)
+
+if number >= 0:
+    last_digit = number % 10
+else:
+    last_digit = -(-number % 10)
 
 if last_digit > 5:
      output = "and is greater than 5"
