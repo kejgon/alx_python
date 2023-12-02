d03667575a3c51bf7bd6b7fc7f27d13cf53ba80e
kejgon james <kejgon@gmail.com>
2023-12-02 18:04:01 +0300

    commit

---
+++ python-functions/test.py
@@ -0,0 +1,12 @@
+
+# counter = 0
+# while counter <= 10:
+#     if counter % 2 == 0:
+#         print(counter)
+#     counter += 1
+
+
+# Practical Slice Email
+print("*" * 73)
+print(("*" * 25) + " " + "Practical Slice Email" + " " +("*" * 25) )
+print("*" * 73)
