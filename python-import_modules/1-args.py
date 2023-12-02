7131937e3984653ec3d7f510e0d9f53248149d55
kejgon james <kejgon@gmail.com>
2023-12-03 00:41:33 +0300

    commit

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,3 +1,4 @@
+# argument_printer.py
 
 import sys
 
@@ -5,8 +6,7 @@ def print_arguments():
     num_arguments = len(sys.argv) - 1
     plural_s = 's' if num_arguments != 1 else ''
 
-    print("{} argument{}:".format(num_arguments, plural_s), end='')
-    print("{}".format('' if num_arguments == 0 else ''))
+    print("{} argument{}.".format(num_arguments, plural_s))
 
     for i, arg in enumerate(sys.argv[1:], start=1):
         print("{}: {}".format(i, arg))
