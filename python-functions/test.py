4cec4e3f9901915342aa1a9c4d408b0caff9a35c
kejgon james <kejgon@gmail.com>
2023-12-02 18:49:51 +0300

    commit

--- python-functions/test.py
+++ python-functions/test.py
@@ -5,18 +5,45 @@
 #         print(counter)
 #     counter += 1
 
+##################################################
+# ############# Practical Slice Email ############
+##################################################
+# print("*" * 73)
+# print(("*" * 25) + " " + "Practical Slice Email" + " " +("*" * 25) )
+# print("*" * 73)
 
-# Practical Slice Email
+# theName = input('What\' your name? ').strip().capitalize()
+# theEmail = input('What\' your email address? ').strip()
+
+# # Here We Want to Get the Username and There Website
+# theUsername = theEmail[:theEmail.index("@")]
+# theWebsite = theEmail[theEmail.index("@") + 1:]
+
+# print(f"Hello {theName}, your email address is {theEmail}")
+# print(f"Your username is {theUsername}, \nyour website is {theWebsite}")
+
+
+##################################################
+# ############# Your Age Full Details ############
+##################################################
 print("*" * 73)
-print(("*" * 25) + " " + "Practical Slice Email" + " " +("*" * 25) )
+print(("*" * 25) + " " + "Your Age Full Details" + " " +("*" * 25) )
 print("*" * 73)
 
-theName = input('What\' your name? ').strip().capitalize()
-theEmail = input('What\' your email address? ').strip()
+getAge = int(input("Enter your age: ").strip())
 
-# Here We Want to Get the Username and There Website
-theUsername = theEmail[:theEmail.index("@")]
-theWebsite = theEmail[theEmail.index("@") + 1:]
+# Get the from Different Units
+months = getAge * 12
+weeks = months * 4
+days = getAge * 365
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
 
-print(f"Hello {theName}, your email address is {theEmail}")
-print(f"Your username is {theUsername}, \nyour website is {theWebsite}")
+print(f"You\' lived for :")
+print(f"Months : {months}")
+print(f"Weeks : {weeks:,}")
+print(f"Days : {days:,}")
+print(f"Hours : {hours:,}")
+print(f"Minutes : {minutes:,}")
+print(f"Seconds : {seconds:,}")
