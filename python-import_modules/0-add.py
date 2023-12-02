66a3ed2f88d59c8e7eddfed81a8617c98a474a4d
kejgon james <kejgon@gmail.com>
2023-12-02 23:29:30 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,22 +1,9 @@
 
-
 a = 1
 b = 2
 
 def add(a, b):
-    """My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+    return a + b
 
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
- 
-
-
