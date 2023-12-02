41b34695818d80747c0e441443b1b744d1cf5c4b
kejgon james <kejgon@gmail.com>
2023-12-03 00:29:08 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -9,5 +9,4 @@ from add_0 import add
 
 if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, add(a, b)))
-    # You can include any additional code that should only run
-    # when the module is executed directly
+    
