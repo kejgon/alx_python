5e2e1505ba9913df4964656b5edb5ddb9db24217
kejgon james <kejgon@gmail.com>
2023-12-02 23:24:28 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,15 @@
 
-# Assign values to variables a and b
+
 a = 1
 b = 2
 
 def add(a, b):
     return a + b
 
+# This block ensures that the code below is only executed
+# when the module is run as the main program, not when imported
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
+ 
 
 
-add(a, b)
-
-print("{} + {} = {}".format(a, b, add(a, b)))
