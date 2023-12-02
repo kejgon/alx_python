2f5705e2049a7496e275916a30f894e584b7f361
kejgon james <kejgon@gmail.com>
2023-12-02 23:48:51 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,18 @@
 # 0-add.py
 
+# main_program.py
+
+# Assign values to variables a and b
+a = 1
+b = 2
+
+
+
+
 def add(a, b):
     return a + b
+
+
+if __name__ == "__main__":
+
+    print("{} + {} = {}".format(a, b, add(a, b)))
