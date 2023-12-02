3ac149175cb2f1e8340ced18282381b6805e8a1d
kejgon james <kejgon@gmail.com>
2023-12-02 18:22:03 +0300

    commit

--- python-functions/test.py
+++ python-functions/test.py
@@ -10,3 +10,13 @@
 print("*" * 73)
 print(("*" * 25) + " " + "Practical Slice Email" + " " +("*" * 25) )
 print("*" * 73)
+
+theName = input('What\' your name? ').strip().capitalize()
+theEmail = input('What\' your email address? ').strip()
+
+# Here We Want to Get the Username and There Website
+theUsername = theEmail[:theEmail.index("@")]
+theWebsite = theEmail[theEmail.index("@") + 1:]
+
+print(f"Hello {theName}, your email address is {theEmail}")
+print(f"Your username is {theUsername}, \nyour website is {theWebsite}")
