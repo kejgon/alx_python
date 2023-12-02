5e34671e8b0d10d28484c3919efb8263f94783db
kejgon james <kejgon@gmail.com>
2023-12-02 19:03:26 +0300

    commit

--- python-functions/test.py
+++ python-functions/test.py
@@ -23,27 +23,47 @@
 # print(f"Your username is {theUsername}, \nyour website is {theWebsite}")
 
 
+# ##################################################
+# # ############# Your Age Full Details ############
+# ##################################################
+# print("*" * 73)
+# print(("*" * 25) + " " + "Your Age Full Details" + " " +("*" * 25) )
+# print("*" * 73)
+
+# getAge = int(input("Enter your age: ").strip())
+
+# # Get the from Different Units
+# months = getAge * 12
+# weeks = months * 4
+# days = getAge * 365
+# hours = days * 24
+# minutes = hours * 60
+# seconds = minutes * 60
+
+# print(f"You\' lived for :")
+# print(f"Months : {months}")
+# print(f"Weeks : {weeks:,}")
+# print(f"Days : {days:,}")
+# print(f"Hours : {hours:,}")
+# print(f"Minutes : {minutes:,}")
+# print(f"Seconds : {seconds:,}")
+
+
 ##################################################
-# ############# Your Age Full Details ############
+# ############# Make Decision ############
 ##################################################
 print("*" * 73)
-print(("*" * 25) + " " + "Your Age Full Details" + " " +("*" * 25) )
+print(("*" * 25) + " " + "Make Decision" + " " +("*" * 25) )
 print("*" * 73)
 
-getAge = int(input("Enter your age: ").strip())
+uName = "Kejgon James"
+uCountry = "South Korea"
+cName = "Python"
+cPrice = 100
 
-# Get the from Different Units
-months = getAge * 12
-weeks = months * 4
-days = getAge * 365
-hours = days * 24
-minutes = hours * 60
-seconds = minutes * 60
-
-print(f"You\' lived for :")
-print(f"Months : {months}")
-print(f"Weeks : {weeks:,}")
-print(f"Days : {days:,}")
-print(f"Hours : {hours:,}")
-print(f"Minutes : {minutes:,}")
-print(f"Seconds : {seconds:,}")
+if uCountry == "South Sudan":
+    print(f"Hi {uName}, because you're from {uCountry}!")
+    print(f"The course \'{cName}\' Price is : ${cPrice - 95}.")
+else:
+    print(f"Hi {uName}, because you're from {uCountry}!")
+    print(f"The course \'{cName}\' Price is : ${cPrice - 50}.")
