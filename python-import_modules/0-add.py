51fbc0d4ad419fa27fac6b570241335b822317d8
kejgon james <kejgon@gmail.com>
2023-12-03 00:21:28 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,13 @@
+
+
 a = 1
 b = 2
 
 
-def add_0(a, b):
-    return a + b
+from add_0 import add
+
 
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add_0(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
     # You can include any additional code that should only run
     # when the module is executed directly
