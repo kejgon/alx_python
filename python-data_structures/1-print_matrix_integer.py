d17aa76f8633a8575884f2a0798cd72c88a63ffa
kejgon james <kejgon@gmail.com>
2023-12-03 22:21:35 +0300

    commit

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,11 @@
 def print_matrix_integer(matrix=[]):
     for row in matrix:
-        print(" ".join(map(str, row)))
+        for num in row:
+            print("{:d}".format(num), end=" ")
+        print()
 
 # Example usage
 if __name__ == "__main__":
-    matrix = []
 
-    print_matrix_integer(matrix)
-    print("--")
     print_matrix_integer()
+  
