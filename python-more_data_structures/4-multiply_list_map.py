5d9ee69f7f77e6870b8c9f61d3835ae937dd3069
kejgon james <kejgon@gmail.com>
2023-12-07 09:44:02 +0300

    commit

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,9 +1,1 @@
-def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
-
-
-# if __name__ == "__main__":
-#     my_list = [1, 2, 3, 4, 6]
-#     new_list = multiply_list_map(my_list, 4)
-#     print(new_list)
-#     print(my_list)
+def multiply_list_map(my_list=[], number=0): return list(map(lambda x: x * number, my_list))
