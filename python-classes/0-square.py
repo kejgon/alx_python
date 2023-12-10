3d530db634af363aca28bd6a6df3aa8667896211
kejgon james <kejgon@gmail.com>
2023-12-10 15:43:41 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,18 @@
 class Square:
-
-    def __init__(self, size=3):
+    def __init__(self, size):
         self.__size = size
 
-    def my_square(self, x):
-        return x ** 2
-
+# Test cases
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
 
 try:
     print(my_square.size)
-except Exception as e:
+except AttributeError as e:
     print(e)
 
 try:
     print(my_square.__size)
-except Exception as e:
+except AttributeError as e:
     print(e)
