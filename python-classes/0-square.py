a237ee52199d34533b6ffd789e3606d3c5372009
kejgon james <kejgon@gmail.com>
2023-12-10 15:09:41 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -7,31 +7,31 @@ class Square:
         return x ** 2
 
 # Create an instance of the Square class
-mysquare = Square()
-# Correct output - case: mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
+mysquare1 = Square(3) # Correct output - case: mysquare = Square(3)
+print(type(mysquare1))
+print(mysquare1.__dict__)
 
-# Correct output - case: mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
 
+
+mysquare2 = Square(89)
 # mysquare = Square(89)
-print(type(mysquare))
-print(mysquare.__dict__)
+print(type(mysquare2))
+print(mysquare2.__dict__)
+
+mysquare3 = Square(3) # Correct output - case: mysquare = Square(3)
 
 # Correct output - case: mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
+print(type(mysquare3))
+print(mysquare3.__dict__)
 
 # Try to access the non-existent attribute 'size'
 try:
-    print(mysquare.size)
+    print(mysquare3.size)
 except AttributeError as e:
     print(e)
 
 # Try to access the non-mangled attribute '_size'
 try:
-    print(mysquare._size)
+    print(mysquare3._size)
 except AttributeError as e:
     print(e)
