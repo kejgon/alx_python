f5616a263a199f1ec1e3e8d5d4c5213dc4e4f58f
kejgon james <kejgon@gmail.com>
2023-12-12 01:26:16 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,12 +2,8 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-# Correct output - case: mysquare = Square(3)
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
 
-# Correct output - case: mysquare = Square(3)
+
 mysquare = Square(3)
 print(type(mysquare))
 print(mysquare.__dict__)
@@ -17,19 +13,13 @@ mysquare = Square(89)
 print(type(mysquare))
 print(mysquare.__dict__)
 
-# Correct output - case: mysquare = Square(3)
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
 
-# Try to access the non-existent attribute 'size'
-try:
-    print(mysquare.size)
-except AttributeError as e:
-    print(e)
+# try:
+#     print(mysquare.size)
+# except AttributeError as e:
+#     print(e)
 
-# Try to access the non-mangled attribute '_size'
-try:
-    print(mysquare._Square__size)
-except AttributeError as e:
-    print(e)
+# try:
+#     print(mysquare._Square__size)
+# except AttributeError as e:
+#     print(e)
