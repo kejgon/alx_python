24c972ba84f00577d0fc96315ebe065955f20b9c
kejgon james <kejgon@gmail.com>
2023-12-12 01:36:39 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,22 +1,3 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-
-# Create an instance of the Square class
-mysquare = Square(3)
-print(type(mysquare))
-
-# Access the __size attribute directly
-print(mysquare._Square__size)
-
-# Attempt to access the 'dict_' attribute (which doesn't exist)
-try:
-    print(mysquare.dict_)
-except AttributeError as e:
-    print(e)
-
-# Attempt to access the 'size' attribute (which doesn't exist)
-try:
-    print(mysquare.size)
-except AttributeError as e:
-    print(e)
