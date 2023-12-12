dcff8171911497fe297acd50cfdd64e027dd8ac7
kejgon james <kejgon@gmail.com>
2023-12-12 18:31:48 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,19 +2,3 @@ class Square:
     def __init__(self, size=3):
         self.__size = size
 
-
-Square = __import__('0-square').Square
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)
-except Exception as e:
-    print(e)
