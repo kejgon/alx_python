c965d9dc699671cce19035a4947425cf6e440266
kejgon james <kejgon@gmail.com>
2023-12-12 18:35:47 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,25 @@
 class Square:
-    def __init__(self, size=3):
-        self.__size = size
+    """A class representing a square.
 
+    The Square class is designed to create square objects with a specified size.
+    It initializes the size attribute during object creation.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        No additional methods are provided in this simple example.
+
+    Example:
+        >>> square = Square(5)
+        >>> print(square.__size)
+        5
+    """
+
+    def __init__(self, size=3):
+        """Initialize a Square object with a specified size.
+
+        Args:
+            size (int): The size of the square. Defaults to 3.
+        """
+        self.__size = size
