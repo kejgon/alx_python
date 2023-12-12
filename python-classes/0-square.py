35bab1d1633ca7ca3e42499736ddf547dedd4954
kejgon james <kejgon@gmail.com>
2023-12-12 12:10:45 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,18 +5,20 @@ class Square:
 
 
 
-# my_square = Square(3)
 
-# print(type(my_square))
 
-# print(my_square.__dict__)
+my_square = Square(3)
 
-# # try:
-#      print(my_square.size)
-# except AttributeError as e:
-#     print(e)
+print(type(my_square))
 
-# try:
-#     print(my_square.__size)
-# except AttributeError as e:
-#     print(e)
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except AttributeError as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except AttributeError as e:
+    print(e)
