0f255e8670e61f71d3b183ffec741593023ce58e
kejgon james <kejgon@gmail.com>
2023-12-12 11:28:18 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,24 +2,3 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
-# Example usage
-if __name__ == "__main__":
-    my_square = Square(3)
-
-    # Output: <class '__main__.Square'>
-    print(type(my_square))
-
-    # Output: {'_Square__size': 3}
-    print(my_square.__dict__)
-
-    # Output: 'Square' object has no attribute 'size'
-    try:
-        print(my_square.size)
-    except AttributeError as e:
-        print(e)
-
-    # Output: 'Square' object has no attribute '__size'
-    try:
-        print(my_square.__size)
-    except AttributeError as e:
-        print(e)
--- python-classes/main.py
+++ python-classes/main.py
@@ -1,11 +1,25 @@
 
 Square = __import__('0-square').Square
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
 
-try:
-    print(my_square._Square__size)
-except Exception as e:
-    print(e)
+# Example usage
+if __name__ == "__main__":
+    my_square = Square(3)
+
+    # Output: <class '__main__.Square'>
+    print(type(my_square))
+
+    # Output: {'_Square__size': 3}
+    print(my_square.__dict__)
+
+    # Output: 'Square' object has no attribute 'size'
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
+
+    # Output: 'Square' object has no attribute '__size'
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)
