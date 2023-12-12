f6977c41912871fa5941072859af15c118bcb1de
kejgon james <kejgon@gmail.com>
2023-12-12 11:32:48 +0300

    commit

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,3 +2,20 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
+
+
+my_square = Square(3)
+
+print(type(my_square))
+
+print(my_square.__dict__)
+
+try:
+     print(my_square.size)
+except AttributeError as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except AttributeError as e:
+    print(e)
--- python-classes/main.py
+++ python-classes/main.py
@@ -2,23 +2,18 @@
 Square = __import__('0-square').Square
 
 
-# Example usage
 if __name__ == "__main__":
     my_square = Square(3)
 
-    # Output: <class '__main__.Square'>
     print(type(my_square))
 
-    # Output: {'_Square__size': 3}
     print(my_square.__dict__)
 
-    # Output: 'Square' object has no attribute 'size'
     try:
         print(my_square.size)
     except AttributeError as e:
         print(e)
 
-    # Output: 'Square' object has no attribute '__size'
     try:
         print(my_square.__size)
     except AttributeError as e:
