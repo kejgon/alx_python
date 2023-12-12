e3d0b3f35496ab4bb22081657d8627fd6ebb375d
kejgon james <kejgon@gmail.com>
2023-12-12 11:57:10 +0300

    commit

---
+++ python-classes/main_0.py
@@ -0,0 +1,27 @@
+
+Square = __import__('0-square').Square
+
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+
+# Square = __import__('0-square').Square
+
+
+# if __name__ == "__main__":
+#     my_square = Square(3)
+
+#     print(type(my_square))
+
+#     print(my_square.__dict__)
+
+#     try:
+#         print(my_square.size)
+#     except AttributeError as e:
+#         print(e)
+
+#     try:
+#         print(my_square.__size)
+#     except AttributeError as e:
+#         print(e)
