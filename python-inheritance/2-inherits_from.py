6ff9f6d312c1c63b3dc513a280f3c806832d86d2
kejgon james <kejgon@gmail.com>
2023-12-14 10:56:25 +0300

    commit

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,6 @@
+"""
+    Check if the object is an instance of a class that inherited from the specified class.
+"""
+def inherits_from(obj, a_class):
+    """Check if the object is an instance of a class that inherited from the specified class."""
+    return issubclass(type(obj), a_class)
