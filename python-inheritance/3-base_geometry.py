edce5c9e546b9ab2f8c64fbdc88e16b9ace6dfa8
kejgon james <kejgon@gmail.com>
2023-12-14 11:12:02 +0300

    commit

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,5 +4,11 @@ This module defines the BaseGeometry class.
 The BaseGeometry class is an empty class that will serve as the base class
 for geometry-related classes in the future.
 """
+
 class BaseGeometry:
+    """
+    The BaseGeometry class is the base class for geometry-related classes.
+
+    It currently does not have any attributes or methods.
+    """
     pass
