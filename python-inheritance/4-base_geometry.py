525a8439bb792f24d8ae986b54e8f76e5f0d2580
kejgon james <kejgon@gmail.com>
2023-12-14 11:24:07 +0300

    commit

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -19,3 +19,8 @@ class BaseGeometry:
             Exception: This method is not implemented in the base class.
         """
         raise Exception("area() is not implemented")
+
+# Filter out the automatically added methods in Python 3.6+
+attributes = [attr for attr in dir(BaseGeometry) if not callable(getattr(BaseGeometry, attr))]
+
+# print(attributes)
