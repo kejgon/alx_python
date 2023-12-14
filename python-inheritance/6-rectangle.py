fde0fd0325db87702802672d24b2ca14de6cbaff
kejgon james <kejgon@gmail.com>
2023-12-14 12:24:39 +0300

    commit

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -4,8 +4,6 @@
 # base_geometry.py
 class BaseGeometry:
     """
-    The BaseGeometry class is the base class for geometry-related classes.
-
     It includes methods for calculating area and validating integer values.
     """
 
