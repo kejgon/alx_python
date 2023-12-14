76e96a263b41fd549a3eb1b0123aa8e15aa25d7d
kejgon james <kejgon@gmail.com>
2023-12-14 12:04:42 +0300

    commit

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -34,3 +34,18 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+    def __dir__(self):
+        """
+        Customize the dir() output to specify the order of attributes.
+
+        Returns:
+            list: A sorted list of attribute names.
+        """
+        return sorted(['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+                       '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+                       '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
+                       '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+                       'area', 'integer_validator'])
+
+
+
