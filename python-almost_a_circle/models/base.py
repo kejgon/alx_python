258280e40b0422cd7faf585cc176cdf8afd44975
kejgon james <kejgon@gmail.com>
2023-12-17 18:23:10 +0300

    commit

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,3 +1,6 @@
+"""
+    Base Class initialization
+"""
 class Base:
     """Base class for managing id attribute in future classes."""
 
@@ -17,18 +20,18 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-# if __name__ == "__main__":
-#     b1 = Base()
-#     print(b1.id)
+if __name__ == "__main__":
+    b1 = Base()
+    print(b1.id)
 
-#     b2 = Base()
-#     print(b2.id)
+    b2 = Base()
+    print(b2.id)
 
-#     b3 = Base()
-#     print(b3.id)
+    b3 = Base()
+    print(b3.id)
 
-#     b4 = Base(12)
-#     print(b4.id)
+    b4 = Base(12)
+    print(b4.id)
 
-#     b5 = Base()
-#     print(b5.id)
+    b5 = Base()
+    print(b5.id)
