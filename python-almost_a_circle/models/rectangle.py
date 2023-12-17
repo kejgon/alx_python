600a901d05621c6ba8d67a7c937b66aeb9e5a2db
kejgon james <kejgon@gmail.com>
2023-12-18 00:32:59 +0300

    commit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,8 +87,10 @@ class Rectangle(Base):
 
     def display(self):
         """Prints the Rectangle instance with the character '#'."""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Returns a string representation of the Rectangle instance."""
