7e112c84563d0ef27e25b1294fa6e21885a6c693
kejgon james <kejgon@gmail.com>
2023-12-22 23:08:12 +0300

    commit

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 import sys
+import base64
 
 if len(sys.argv) != 3:
     print("Usage: {} <username> <token>".format(sys.argv[0]))
@@ -9,8 +10,11 @@ if len(sys.argv) != 3:
 username = sys.argv[1]
 token = sys.argv[2]
 
+# Combine username and token with a colon and encode in base64
+credentials = base64.b64encode('{}:{}'.format(username, token).encode('utf-8')).decode('utf-8')
+
 url = "https://api.github.com/user"
-headers = {'Authorization': 'token ' + token}
+headers = {'Authorization': 'Basic ' + credentials}
 
 try:
     response = requests.get(url, headers=headers)
