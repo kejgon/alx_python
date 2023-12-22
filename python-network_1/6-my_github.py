b7fdef7df21532566df5d7145d72608bfdb13c77
kejgon james <kejgon@gmail.com>
2023-12-22 22:51:38 +0300

    commit

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,19 @@
+import requests
+import sys
+
+if len(sys.argv) != 3:
+    print("Usage: {} <username> <token>".format(sys.argv[0]))
+    sys.exit(1)
+
+username = sys.argv[1]
+token = sys.argv[2]
+
+url = "https://api.github.com/user"
+headers = {'Authorization': 'Basic ' + '{}:{}'.format(username, token).encode('base64')}
+
+try:
+    response = requests.get(url, headers=headers)
+    user_data = response.json()
+    print(user_data.get('id', 'None'))
+except ValueError:
+    print('None')
