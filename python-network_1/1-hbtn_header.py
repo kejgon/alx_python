dfceeff95a6c9fe43b3c8a906b4f428fc62621f1
kejgon james <kejgon@gmail.com>
2023-12-22 22:20:03 +0300

    commit

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: {} <URL>".format(sys.argv[0]))
+    sys.exit(1)
+
+url = sys.argv[1]
+response = requests.get(url)
+
+x_request_id = response.headers.get('X-Request-Id')
+print(x_request_id)
