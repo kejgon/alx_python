943e0b975e0981a6042dc9fd4354183bd9f8de0d
kejgon james <kejgon@gmail.com>
2024-01-18 13:52:35 +0300

    commit

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,25 @@
-""" Importing  SQLAlchemy
-"""
+"""Importing SQLAlchemy"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-"""  Creating an Object
-"""
+
+"""Creating a Base object"""
 Base = declarative_base()
 
-"""  Creating class State 
-"""
+"""Creating the State class"""
 class State(Base):
+    """Class representing the 'states' table in the database."""
     __tablename__ = 'states'
+    
+    # Columns
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-"""  Main section 
-"""
+"""Main section"""
 if __name__ == "__main__":
-    # Assuming MySQL server is running on localhost with default port 3306
+    # Connecting to the MySQL server
+    # Replace 'kejgon', 'Password', 'localhost:3306', and 'hbtn_test_db_5' with your actual credentials and database information
     engine = create_engine('mysql+mysqldb://kejgon:Password@localhost:3306/hbtn_test_db_5')
+    
+    # Creating tables in the database
     Base.metadata.create_all(engine)
