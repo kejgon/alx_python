197debb1f52a9c70a1f91bbbb54ee6810974c03c
kejgon james <kejgon@gmail.com>
2024-01-18 16:54:54 +0300

    commit

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,78 +1,19 @@
-"""
-    The BaseGeometry class is the base class for geometry-related classes.
-"""
-# base_geometry.py
-class BaseGeometry:
-    """
-    The BaseGeometry class is the base class for geometry-related classes.
-    """
-
-    def area(self):
-        """
-        Public instance method to calculate the area.
-
-        Raises:
-            Exception: This method is not implemented in the base class.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        Validate an integer value.
-
-        Args:
-            name (str): The name of the value being validated.
-            value (int): The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+"""class Rectangle inheriting from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """
-    The Rectangle class represents a rectangle and inherits from BaseGeometry.
-
-    Attributes:
-        __width (int): The width of the rectangle.
-        __height (int): The height of the rectangle.
-    """
-
+    """principal function"""
     def __init__(self, width, height):
-        """
-        Initialize a Rectangle instance with the specified width and height.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
         self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
 
+    """public method def area"""
     def area(self):
-        """
-        Calculate the area of the rectangle.
-
-        Returns:
-            int: The calculated area.
-        """
         return self.__width * self.__height
 
+    """public method magic"""
     def __str__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-            str: The formatted string representation.
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-
-
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
