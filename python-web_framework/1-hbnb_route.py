312c8e5aaa41c6c92779734ed3950933558d7286
kejgon james <kejgon@gmail.com>
2024-01-21 23:26:23 +0300

    commit

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -5,8 +5,8 @@ argument -- description
 Return: return_description
 """
 
+from glob import escape
 from flask import Flask
-
 # Create a Flask application instance
 app = Flask(__name__)
 
@@ -29,6 +29,21 @@ def hbnb():
         str: A string containing the "Hello HBNB!" message.
     """
     return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """
+    Route handler for the '/c/<text>' URL.
+
+    Args:
+        text (str): The value of the 'text' variable from the URL.
+
+    Returns:
+        str: A string containing "C " followed by the value of the 'text' variable.
+    """
+    # Replace underscore (_) symbols with a space
+    formatted_text = escape(text.replace('_', ' '))
+    return f"C {formatted_text}"
 
 if __name__ == '__main__':
     # Run the Flask application on 0.0.0.0 (accessible from any network interface) and port 5000
