0fda4b356c03bd0c00f9254c8dd46ab0d8b33a5e
kejgon james <kejgon@gmail.com>
2024-01-21 23:48:16 +0300

    commit

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,94 @@
+"""Importing the Flask Framework
+
+Keyword arguments:
+argument -- description
+Return: return_description
+"""
+from flask import Flask, escape, render_template
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL.
+
+    Returns:
+        str: A string containing the "Hello HBNB!" message.
+    """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the '/hbnb' URL.
+
+    Returns:
+        str: A string containing the "HBNB" message.
+    """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    """
+    Route handler for the '/c/<text>' URL.
+
+    Args:
+        text (str): The value of the 'text' variable from the URL.
+
+    Returns:
+        str: A string containing "C " followed by the value of the 'text' variable.
+    """
+    # Replace underscore (_) symbols with a space
+    formatted_text = escape(text.replace('_', ' '))
+    return f"C {formatted_text}"
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_with_text(text):
+    """
+    Route handler for the '/python/<text>' URL.
+
+    Args:
+        text (str): The value of the 'text' variable from the URL. Defaults to 'is_cool'.
+
+    Returns:
+        str: A string containing "Python " followed by the value of the 'text' variable.
+    """
+    # Replace underscore (_) symbols with a space
+    formatted_text = escape(text.replace('_', ' '))
+    return f"Python {formatted_text}"
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    Route handler for the '/number/<n>' URL.
+
+    Args:
+        n (int): The value of the 'n' variable from the URL.
+
+    Returns:
+        str: A string containing "n is a number" only if n is an integer.
+    """
+    return f"{n} is a number"
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    Route handler for the '/number_template/<n>' URL.
+
+    Args:
+        n (int): The value of the 'n' variable from the URL.
+
+    Returns:
+        str: A HTML page with an H1 tag containing "Number: n" if n is an integer.
+    """
+    if isinstance(n, int):
+        return render_template('number_template.html', n=n)
+    else:
+        return "Not a valid integer"
+
+if __name__ == '__main__':
+    # Run the Flask application on 0.0.0.0 (accessible from any network interface) and port 5000
+    app.run(host='0.0.0.0', port=5000)
