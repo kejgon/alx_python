a77896006394569d458a7404392ed8650d213947
kejgon james <kejgon@gmail.com>
2024-01-24 23:33:38 +0300

    commit

--- python-web_framework/9-update_delete_users.py
+++ python-web_framework/9-update_delete_users.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 import sys
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ if len(sys.argv) != 4:
     print("Usage: python 7-db_setup.py <db_username> <db_password> <db_name>")
     sys.exit(1)
 
+# Retrieve command-line arguments
 db_username = sys.argv[1]
 db_password = sys.argv[2]
 db_name = sys.argv[3]
@@ -20,11 +22,12 @@ db_uri = f"mysql://{db_username}:{db_password}@{db_host}/{db_name}"
 # Configuring Flask app
 app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'your_secret_key'  # Change this to a secure secret key
 
 # Creating SQLAlchemy instance
 db = SQLAlchemy(app)
 
-# Your model class here
+# Your User model class here
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
@@ -37,15 +40,14 @@ def create_tables():
 
 create_tables()  # This calls the function to create tables
 
-# Your routes and other Flask code here
-
 @app.route('/', strict_slashes=False)
 def index():
+    """Route to display a simple greeting."""
     return "Hello, ALX Flask!"
-
 
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user():
+    """Route to add a new user."""
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
@@ -64,12 +66,13 @@ def add_user():
 
 @app.route('/users')
 def display_users():
+    """Route to display all users."""
     users = User.query.all()  # Retrieve all users
     return render_template('users.html', users=users)
 
-
 @app.route('/update_user/<int:user_id>', methods=['GET', 'POST'])
 def update_user(user_id):
+    """Route to update a user."""
     user = User.query.get(user_id)
     if user is None:
         flash("Error: User not found!", 'error')
@@ -95,6 +98,7 @@ def update_user(user_id):
 
 @app.route('/delete_user/<int:user_id>', methods=['GET', 'POST'])
 def delete_user(user_id):
+    """Route to delete a user."""
     user = User.query.get(user_id)
     if user is None:
         flash("Error: User not found!", 'error')
