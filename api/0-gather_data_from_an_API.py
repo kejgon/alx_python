a26990caee31cfb69b71801eeb5242a67babb36f
kejgon james <kejgon@gmail.com>
2024-02-24 21:18:26 +0300

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,32 +1,45 @@
+
 import requests
 import sys
+from sys import argv
 
-def check_tasks(id):
-    """ Fetch user name, number of tasks """
-    
-    # Fetch user name
-    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}')
-    user_data = user_response.json()
-    username = user_data['name']
-    
-    # Fetch tasks
-    tasks_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{id}/todos')
-    tasks_data = tasks_response.json()
-    total_tasks = len(tasks_data)
-    completed_tasks = sum(1 for task in tasks_data if task['completed'])
 
-    print(f"Employee {username} is done with tasks({completed_tasks}/{total_tasks}):")
+def get_employee(sizeofReq):
+    """ Use API from jsonplaceholder """
 
-    # Print titles of completed tasks and check formatting
-    for count, task in enumerate(tasks_data, start=1):
-        if task['completed']:
-            if task['title'].startswith('\t ') and task['title'].endswith('\n'):
-                print(f"\t{task['title']}")
-                print(f"Task {count} Formatting: OK")
-            else:
-                print(f"Task {count} Formatting: Incorrect")
-        else:
-            print(f"Task {count} Formatting: OK")
+    # Variables
+    taskList = []
+    count = 0
 
-if __name__ == "__main__":
-    check_tasks(int(sys.argv[1]))
+    link = "https://jsonplaceholder.typicode.com"
+
+    # get requests
+    usersRes = requests.get(
+        "{}/users/{}".format(link, sizeofReq))
+    todosRes = requests.get(
+        "{}/users/{}/todos".
+        format(link, sizeofReq))
+
+    # Get the json from responses
+    name = usersRes.json().get('name')
+    todosJson = todosRes.json()
+
+    # Save the employee Name -- Loop the tasks
+    for task in todosJson:
+        if task.get('completed') is True:
+            count += 1
+            # save the task title to taskList
+            taskList.append(task.get('title'))
+
+    # Print the first line
+    print('Employee {} is done with tasks({}/{}):'.format(
+        name, count, len(todosJson)))
+    # Loop the taskList and print tasks
+    for title in taskList:
+        print('\t {}'.format(title))
+
+    return 0
+
+
+if __name__ == '__main__':
+    get_employee(argv[1])
